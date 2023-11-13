ada92b580a83c58047de353ae46e1429a72a6b94
bube <attahgbube@gmail.com>
2023-11-13 15:30:56 +0100

    -

--- src/state-manager/store.js
+++ src/state-manager/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import generalReducer from "./slices/general/generalSlice"
+import generalReducer from "./slices/general/generalSlice";
 
 export const store = configureStore({
   reducer: {
-    general: generalReducer
+    general: generalReducer,
   },
 });
